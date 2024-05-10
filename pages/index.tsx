ace0fd3068af569a021c3b69501b341d8fc4923b
Noah <67226652+Noahmpr@users.noreply.github.com>
2024-05-10 07:50:34 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
         const conf = document.createElement('script')
         const script = document.createElement('script')
         script.type = 'text/javascript'
-        script.src = `//www.highperformancedformats.com/${atOptions.key}/invoke.js`
+        script.src = `//www.topcreativeformat.com/${atOptions.key}/invoke.js`
         conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
 
         banner.current.append(conf)
