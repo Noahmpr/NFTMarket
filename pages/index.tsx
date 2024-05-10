a4eaf766aa3901af43857a0725410b6d66eca131
Noah <67226652+Noahmpr@users.noreply.github.com>
2024-05-10 08:21:50 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,13 @@
 
 import type { NextPage } from "next";
 import Link from "next/link";
-import { useEffect, useRef } from 'react';
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Head from 'next/head';
 
 const Home: NextPage = () => {
-const banner = useRef();
 
-const atOptions = {
-key: '6481894a0fe00fac4198f25c96514aea',
-format: 'iframe',
-height: 50,
-width: 320,
-params: {},
-};
-	
- useEffect(() => {
 
-const conf = document.createElement('script');
-const script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = `//www.topcreativeformat.com/${atOptions.key}/invoke.js`;
-conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
-
-banner.current.appendChild(conf);
-banner.current.appendChild(script);
-}, [banner]);
-	
   return (
     <>
 <Head>
@@ -105,7 +84,7 @@ var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l);
                   GitHub
                 </Link>
                   <div id="pos-article-display-96542"></div>
-		      <div className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center" ref={banner}></div>
+		 
               </div>
             </div>
           </div>
