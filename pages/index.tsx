06f06ca413d36ffaf0443933e5224b15408528c5
Noah <67226652+Noahmpr@users.noreply.github.com>
2024-12-18 11:43:06 +0330

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,88 +1,99 @@
-
-import type { NextPage } from "next";
+import { NextPage } from "next";
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Head from 'next/head';
 
 const Home: NextPage = () => {
-
-
   return (
     <>
-<Head>
-{/* Yektanet Analytics Script */}
-<script id="chatbot-widget-script" type="text/javascript" src="https://dev.hamhoush.ir/chatbot-widget.js" data-id="67443037c140b189e09afd25" data-token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InRlc3RAZ21haWwuY29tIiwiZW1haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsIm5hbWVpZCI6IjcwM2MxNmI1LWIzY2UtNDMwNC1iZTVlLTNkYzIwOTM2ZjQ4NSIsInRlbmFudE5hbWUiOiJkZXZlbG9wIiwidGVuYW50SWQiOiIxIiwicm9sZSI6IlVTRVIiLCJuYmYiOjE3MzQ0NDIzOTMsImV4cCI6MTczNTA0NzE5MywiaWF0IjoxNzM0NDQyMzkzLCJpc3MiOiJlbnRlcnByaXNlLWlkZW50aXR5IiwiYXVkIjoiQWRtaW5Qb3J0YWwifQ.3bnxRqMZsy2ArA8T7bjg9n8uGzxA6RUqJnpVW4mElPM"></script>
+      <Head>
+        <script
+          id="chatbot-widget-script"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var s = document.createElement('script');
+                s.src = "https://dev.hamhoush.ir/chatbot-widget.js";
+                s.type = "text/javascript";
+                s.dataset.id = "67443037c140b189e09afd25";
+                s.dataset.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InRlc3RAZ21haWwuY29tIiwiZW1haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsIm5hbWVpZCI6IjcwM2MxNmI1LWIzY2UtNDMwNC1iZTVlLTNkYzIwOTM2ZjQ4NSIsInRlbmFudE5hbWUiOiJkZXZlbG9wIiwidGVuYW50SWQiOiIxIiwicm9sZSI6IlVTRVIiLCJuYmYiOjE3MzQ0NDIzOTMsImV4cCI6MTczNTA0NzE5MywiaWF0IjoxNzM0NDQyMzkzLCJpc3MiOiJlbnRlcnByaXNlLWlkZW50aXR5IiwiYXVkIjoiQWRtaW5Qb3J0YWwifQ.3bnxRqMZsy2ArA8T7bjg9n8uGzxA6RUqJnpVW4mElPM";
+                document.head.appendChild(s);
+              })();
+            `,
+          }}
+        />
+      </Head>
 
-</Head>
-
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.hero}>
-          <div className={styles.heroBackground}>
-            <div className={styles.heroBackgroundInner}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.hero}>
+            <div className={styles.heroBackground}>
+              <div className={styles.heroBackgroundInner}>
+                <Image
+                  src="/hero-gradient.png"
+                  width={1390}
+                  height={1390}
+                  alt="Background gradient from red to blue"
+                  quality={100}
+                  className={styles.gradient}
+                />
+              </div>
+            </div>
+            <div className={styles.heroAssetFrame}>
               <Image
-                src="/hero-gradient.png"
-                width={1390}
-                height={1390}
-                alt="Background gradient from red to blue"
+                src="/hero-asset.png"
+                width={860}
+                height={540}
+                alt="Hero asset, NFT marketplace"
                 quality={100}
-                className={styles.gradient}
+                className={styles.heroAsset}
               />
             </div>
-          </div>
-          <div className={styles.heroAssetFrame}>
-            <Image
-              src="/hero-asset.png"
-              width={860}
-              height={540}
-              alt="Hero asset, NFT marketplace"
-              quality={100}
-              className={styles.heroAsset}
-            />
-          </div>
-          <div className={styles.heroBodyContainer}>
-            <div className={styles.heroBody}>
-              <h1 className={styles.heroTitle}>
-                <span className={styles.heroTitleGradient}>
-                  Build NFT Marketplaces
-                </span>
-                <br />
-                faster than ever.
-              </h1>
-              <p className={styles.heroSubtitle}>
-                <Link
-                  className={styles.link}
-                  href="https://thirdweb.com"
-                  target="_blank"
-                >
-                  thirdweb
-                </Link>{" "}
-                gives you the tools you need to create audited, performant, and
-                flexible NFT marketplaces in <b>hours</b>, <i>not months</i>.
-              </p>
+            <div className={styles.heroBodyContainer}>
+              <div className={styles.heroBody}>
+                <h1 className={styles.heroTitle}>
+                  <span className={styles.heroTitleGradient}>
+                    Build NFT Marketplaces
+                  </span>
+                  <br />
+                  faster than ever.
+                </h1>
+                <p className={styles.heroSubtitle}>
+                  <Link
+                    className={styles.link}
+                    href="https://thirdweb.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    thirdweb
+                  </Link>{" "}
+                  gives you the tools you need to create audited, performant,
+                  and flexible NFT marketplaces in <b>hours</b>,{" "}
+                  <i>not months</i>.
+                </p>
 
-              <div className={styles.heroCtaContainer}>
-                <Link className={styles.heroCta} href="/buy">
-                  Get Started
-                </Link>
-                <Link
-                  className={styles.secondaryCta}
-                  href="https://github.com/amirhosseinmpr/NFTMarket"
-                  target="_blank"
-                >
-                  GitHub
-                </Link>
+                <div className={styles.heroCtaContainer}>
+                  <Link className={styles.heroCta} href="/buy">
+                    Get Started
+                  </Link>
+                  <Link
+                    className={styles.secondaryCta}
+                    href="https://github.com/amirhosseinmpr/NFTMarket"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GitHub
+                  </Link>
                   <div id="pos-article-display-96542"></div>
-		 
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    
-          </>
+    </>
   );
 };
 
